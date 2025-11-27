0f044a6fcad882dad17aa6c917623c0b59ad66e1
Italo Rômulo <italomelo39@hotmail.com>
2025-11-27 15:18:28 -0300

    Refactor gerenciar_carrinho.php for clarity

--- gerenciar_carrinho.php
+++ gerenciar_carrinho.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
 
-// Se não tiver logado, não mexe no carrinho
 if (!isset($_SESSION['id_usuario'])) {
     header("Location: login.php");
     exit;
 }
 
-// Inicializa carrinho se não existir
 if (!isset($_SESSION['carrinho'])) {
     $_SESSION['carrinho'] = [];
 }
@@ -18,7 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
         $acao = $_POST['acao'];
         
-        // AÇÃO: ADICIONAR
         if ($acao == 'adicionar' && isset($_POST['id_prod'])) {
             $id = intval($_POST['id_prod']);
             if (!isset($_SESSION['carrinho'][$id])) {
@@ -27,8 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $_SESSION['carrinho'][$id] += 1;
             }
         }
-        
-        // AÇÃO: REMOVER UM ITEM (Diminuir quantidade)
+
         if ($acao == 'remover_um' && isset($_POST['id_prod'])) {
             $id = intval($_POST['id_prod']);
             if (isset($_SESSION['carrinho'][$id])) {
@@ -39,7 +35,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
 
-        // AÇÃO: REMOVER PRODUTO INTEIRO
         if ($acao == 'remover_produto' && isset($_POST['id_prod'])) {
             $id = intval($_POST['id_prod']);
             if (isset($_SESSION['carrinho'][$id])) {
@@ -47,14 +42,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
 
-        // AÇÃO: LIMPAR CARRINHO
         if ($acao == 'limpar') {
             unset($_SESSION['carrinho']);
         }
     }
 }
 
-// Redireciona de volta para a página de onde veio (inicial.php ou carrinho.php)
 if(isset($_SERVER['HTTP_REFERER'])) {
     header("Location: " . $_SERVER['HTTP_REFERER']);
 } else {
