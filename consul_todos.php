0698a623011a5b0dbe28a341638af500d39b3bf2
Italo Rômulo <italomelo39@hotmail.com>
2025-11-27 20:40:09 -0300

    Update consul_todos.php

--- consul_todos.php
+++ consul_todos.php
@@ -11,9 +11,27 @@ $resultado = $conexao->query($sql_consulta);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todos os Produtos Cadastrados</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
+<style>
+    body {
+        background-color: #121212;
+    }
+
+    h2 {
+        color: #ffffff;
+    }
+
+    tbody tr {
+        background-color: #222;
+        color: #ffffff;
+    }
+
+    tbody tr:hover {
+        background-color: #505050;
+    }
+</style>
 
 <body>
 
@@ -28,8 +46,8 @@ $resultado = $conexao->query($sql_consulta);
     <div class="container">
         <div class="row">
             <div class="col">
-                <table class="table table-hover table-striped">
-                    <thead style="background-color: #0d6efd; color: white; border-color: #0a58ca;">
+                <table class="tabela">
+                    <thead>
                         <tr>
                             <th style="color: white !important; background-color: #0d6efd;">Código (ID)</th>
                             <th style="color: white !important; background-color: #0d6efd;">Nome Produto</th>
@@ -72,7 +90,9 @@ $resultado = $conexao->query($sql_consulta);
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
